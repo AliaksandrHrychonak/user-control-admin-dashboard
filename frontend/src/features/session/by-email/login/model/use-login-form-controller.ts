04ecfa446a9fb9ebea4da35fbf47efaaaafda88e
AliaksandrHrychonak <@icloud.com>
2025-03-24 17:30:26 +0300

    fix: refactor error handling

--- frontend/src/features/session/by-email/login/model/use-login-form-controller.ts
+++ frontend/src/features/session/by-email/login/model/use-login-form-controller.ts
@@ -4,8 +4,10 @@ import { useCallback } from 'react';
 import { toast } from 'sonner';
 
 import { useLoginMutation } from '@entities/session';
+import {createMessageBaseQueryError, isFetchBaseQueryError} from "@shared/api";
 
 import type { LoginFormData } from './login.schema';
+import type { IError} from "@shared/api";
 
 interface LoginControllerProps {
     onComplete?: () => void | unknown;
@@ -27,7 +29,13 @@ export const useLoginFormController = ({
                 // TODO Texts should be in the config, need fix after review
                 toast.success('Login successfully');
             } catch (error) {
-                toast.error(`Failed to login: ${error}`);
+                if(isFetchBaseQueryError(error)) {
+                    const errorData = error.data as IError;
+                    const message = createMessageBaseQueryError(errorData?.errors);
+                    toast.error(`${errorData.statusCode || error.status}. Failed to login`,{
+                        description: message || errorData.message
+                    });
+                }
             }
         },
         [login, onComplete]
